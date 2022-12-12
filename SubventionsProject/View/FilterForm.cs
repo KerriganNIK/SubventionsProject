8bde1fa84813cbf38b885653c910cd8d2668fef7
Коршунов Никита <nikita-ko85@mail.ru>
2022-12-12 23:33:41 +0500

    Убрал повторяющиеся значения в "ComboBox" в Fillter

--- SubventionsProject/View/FilterForm.cs
+++ SubventionsProject/View/FilterForm.cs
@@ -4,6 +4,9 @@ using Newtonsoft.Json;
 using SubventionsProject.Data;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace SubventionsProject
 {
@@ -44,14 +47,28 @@ namespace SubventionsProject
 
                 foreach (var items in subventionResponse)
                 {
-                    comboMunicipal.Items.Add(items.Distributor.Name);
-                    comboGetSubvention.Items.Add(items.Receiver.Name);
-                    comboSetSubvention.Items.Add(items.Distributor.Name);
-                    comboYears.Items.Add(items.Year.Year);
+                    comboMunicipal.Items.Add(items.Distributor.Name.ToString());
+                    comboGetSubvention.Items.Add(items.Receiver.Name.ToString());
+                    comboSetSubvention.Items.Add(items.Distributor.Name.ToString());
+                    comboYears.Items.Add(items.Year.Year.ToString());
                     comboSum.Items.Add(items.Sum.ToString());
                     comboDate.Items.Add(items.Year.ToString().Substring(0, 5));
                 }
+
+                ComboBox[] comboBoxs = { comboMunicipal, comboGetSubvention, comboSetSubvention, comboYears, comboSum, comboDate };
+
+                for (int i = 0; i < comboBoxs.Length; i++)
+                {
+                    DistinctData(comboBoxs[i]);
+                }
             }
+        }
+
+        private void DistinctData(ComboBox comboBox)
+        {
+            object[] array = comboBox.Items.OfType<string>().Distinct().ToArray();
+            comboBox.Items.Clear();
+            comboBox.Items.AddRange(array);
         }
     }
 }
